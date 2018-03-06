0cefc89de078bcb89cf681b644d3c95923177667
Ed <ea.litten@gmail.com>
2018-03-06 18:54:37 +0000

    Add basic linear regression model for titanic
    Co-authored-by: reenz <17711442+reenz@users.noreply.github.com>
    Co-authored-by: domvernon <3686359+domvernon@users.noreply.github.com>
    Co-authored-by: Tagrand <24781073+Tagrand@users.noreply.github.com>

--- titanic/titanic_linear_regression.py
+++ titanic/titanic_linear_regression.py
@@ -1,26 +1,23 @@
 import tensorflow as tf
+import titanic_data
 
-def build_model_columns():
-  # Builds a set of feature columns for the model to use
-  age = tf.feature_column.numeric_column('age')
-  pclass = tf.feature_column.numeric_column('pclass')
-  sex = tf.feature_column.categorical_column_with_vocabulary_list(
-    'sex', ['male', 'female']
-  )
+def main(argv):
 
-  feature_columns = [age, pclass, sex]
+  def build_model_columns():
+    # Builds a set of feature columns for the model to use
+    age = tf.feature_column.numeric_column('Age')
+    pclass = tf.feature_column.numeric_column('Pclass')
+    sex = tf.feature_column.categorical_column_with_vocabulary_list(
+      'Sex', ['male', 'female']
+    )
 
-  return feature_columns
+    feature_columns = [age, pclass, sex]
 
-def input_fn():
-  # Generate an input function for the estimator
-  dataset = []
-  return dataset
-
-
-def main():
+    return feature_columns
 
   feature_columns = build_model_columns()
+
+  (train_f, train_l), (test_f, test_l) = titanic_data.load_data()
 
   # Build a Linear classifier which chooses between two classes (survived/died)
   classifier = tf.estimator.LinearClassifier(
@@ -28,11 +25,16 @@ def main():
     n_classes = 2)
 
   classifier.train(
-    input_fn = lambda: input_fn()
+    input_fn = lambda: titanic_data.input_fn_train(train_f, train_l, 50),
+    steps=2000
   )
 
   eval_results = classifier.evaluate(
-    input_fn = lambda: input_fn()
+    input_fn = lambda: titanic_data.input_fn_eval(test_f, test_l, 50)
   )
 
   print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_results))
+
+if __name__ == '__main__':
+  tf.logging.set_verbosity(tf.logging.INFO)
+  tf.app.run(main)
