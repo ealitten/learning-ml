ffb9c8661e206e156ea600011a23be01f069485f
reena <reena.sanjay.sharma@gmail.com>
2018-03-11 19:44:02 +0000

    add null=True, blank=True to type_1,type_2

--- pokemonweb/battle/models.py
+++ pokemonweb/battle/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Pokemon(models.Model):
     uid = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
-    type_1 = models.CharField(max_length=50)
-    type_2 = models.CharField(max_length=50)
+    type_1 = models.CharField(max_length=50,null=True, blank=True)
+    type_2 = models.CharField(max_length=50,null=True, blank=True)
     hp = models.PositiveIntegerField(default=0)
     attack = models.PositiveIntegerField(default=0)
     defense = models.PositiveIntegerField(default=0)
