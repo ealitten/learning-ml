a7f2cfc85f2f8a06c1e1913e073f0c52fdacff54
reena <reena.sanjay.sharma@gmail.com>
2018-03-12 08:58:35 +0000

    added pokemon_data view,form to select a pokemon

---
+++ pokemonweb/battle/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Pokemon
+
+class PokemonForm(forms.ModelForm):
+
+    class Meta:
+        model = Pokemon
+        fields = ['name']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields('name').queryset = Pokemon.objects.none()    
