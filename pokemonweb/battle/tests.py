1661f0668a5a1c07b6d8b2a30fae7454efe328be
reena <reena.sanjay.sharma@gmail.com>
2018-03-12 14:19:10 +0000

    added 1 fake row to pass test on test database

--- pokemonweb/battle/tests.py
+++ pokemonweb/battle/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase, Client
+from .models import Pokemon
 
 import capybara
 import capybara.dsl
 import unittest
 import pokemonweb.wsgi as mywsgi
+
+
 
 capybara.app = mywsgi.application
 capybara.default_driver = "selenium"
@@ -12,15 +15,17 @@ class CapybaraTestCase(unittest.TestCase):
 
     def setUp(self):
         self.page = capybara.dsl.page
+        
 
     def test_battle_page_works(self):
         self.page.visit('index')
         assert self.page.has_text('Welcome to We Predicted That\'s Pokemon Battle!')
 
     def test_pokemon_page_works(self):
+        self.pokemon = Pokemon.objects.create(uid=0, name='Charmander')
         self.page.visit('index')
         self.page.click_link('Your Pokemon Battle')
-        print(page.body)
+        print(self.page.body)
         self.page.select("Charmander", field="pk1")
         
         assert self.page.has_content('Charmander')
