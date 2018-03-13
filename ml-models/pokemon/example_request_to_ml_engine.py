f636bb2746fe1b5380323a5ad3dc1d64b4a7f2ce
Ed <ea.litten@gmail.com>
2018-03-13 13:26:44 +0000

    Test connection to prediction engine

--- ml-models/pokemon/example_request_to_ml_engine.py
+++ ml-models/pokemon/example_request_to_ml_engine.py
@@ -21,20 +21,16 @@ version = 'v10'
 
 service_acc_info = {}
 
-# service_acc_info["type"] = "service_account"
-# service_acc_info["project_id"] = "elegant-beach-197514"
-# service_acc_info["private_key_id"] = os.environ.get("private_key_id")
-service_acc_info["private_key"] = os.environ.get("private_key").replace(r'/\\n/', r'\n')
-service_acc_info["private_key2"] = os.environ.get("private_key").replace(r'/\\n/', '\n')
-service_acc_info["private_key3"] = os.environ.get("private_key").replace('/\\n/', '\n')
-service_acc_info["private_key4"] = os.environ.get("private_key").replace('\\n', '\n')
-service_acc_info["private_key5"] = os.environ.get("private_key").replace(r'\\n', r'\n')
-# service_acc_info["client_email"] = os.environ.get("client_email")
-# service_acc_info["client_id"] = os.environ.get("client_id")
-# service_acc_info["auth_uri"] = "https://accounts.google.com/o/oauth2/auth"
-# service_acc_info["token_uri"] = "https://accounts.google.com/o/oauth2/token"
-# service_acc_info["auth_provider_x509_cert_url"] = "https://www.googleapis.com/oauth2/v1/certs"
-# service_acc_info["client_x509_cert_url"] = os.environ.get("client_x509_cert_url")
+service_acc_info["type"] = "service_account"
+service_acc_info["project_id"] = "elegant-beach-197514"
+service_acc_info["private_key_id"] = os.environ.get("private_key_id")
+service_acc_info["private_key"] = os.environ.get("private_key").replace('\\n', '\n')
+service_acc_info["client_email"] = os.environ.get("client_email")
+service_acc_info["client_id"] = os.environ.get("client_id")
+service_acc_info["auth_uri"] = "https://accounts.google.com/o/oauth2/auth"
+service_acc_info["token_uri"] = "https://accounts.google.com/o/oauth2/token"
+service_acc_info["auth_provider_x509_cert_url"] = "https://www.googleapis.com/oauth2/v1/certs"
+service_acc_info["client_x509_cert_url"] = os.environ.get("client_x509_cert_url")
 
 print(service_acc_info)
 
