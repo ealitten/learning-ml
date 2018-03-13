9047abb7ba5871bec2e75355b8b49d1f2769f3fb
Tagrand <thomas.a.grand@gmail.com>
2018-03-13 11:49:35 +0000

    get pokemon data for ajax request
    Co-authored-by: reenz <17711442+reenz@users.noreply.github.com>
    Co-authored-by: ealitten <ea.litten@gmail.com>

--- battle/urls.py
+++ battle/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('pokemon/', views.pokemon, name='pokemon'),
     path('pokemon_data/', views.pokemon_data, name='pokemon_data'),
     path('pokemon_details/', views.pokemon_details, name='pokemon_details'),
+    path('fight/', views.fight, name='fight')
 ]
 
 # from django.urls import url
--- battle/views.py
+++ battle/views.py
@@ -10,14 +10,13 @@ def pokemon(request):
     return render(request, 'battle/pokemon.html', {})
 
 def pokemon_data(request):
-    all_pokemon = Pokemon.objects.all() 
+    all_pokemon = Pokemon.objects.all()
     selected = request.POST.get('pk1')
     selected_2 = request.POST.get('pk2')
     data = {'all_pokemon': all_pokemon, 'selected': selected, 'selected_2': selected_2}
     return render(request, 'battle/pokemon_data.html', data)
 
 def pokemon_details(request):
-    print("Inside pokemon_details")
     pid = request.GET.get('pid')
     if pid is None:
         return ''
@@ -25,3 +24,12 @@ def pokemon_details(request):
         details = Pokemon.objects.get(uid=pid)
         data = {'details': details}
         return render(request, 'battle/pokemon_details.html', data)
+
+def fight(request):
+    pokemon_1 = request.POST.get('pk1_id')
+    pokemon_2 = request.POST.get('pk2_id')
+    details_1 = Pokemon.objects.get(uid=pokemon_1)
+    details_2 = Pokemon.objects.get(uid=pokemon_2)
+    
+    print(details_1)
+    print(details_2)
