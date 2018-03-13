15f06dac4c0ad3443568e1a90833354b3599155a
Tagrand <thomas.a.grand@gmail.com>
2018-03-13 12:05:35 +0000

    add format of pokemon request
    Co-authored-by: reenz <17711442+reenz@users.noreply.github.com>
    Co-authored-by: ealitten <ea.litten@gmail.com>

--- battle/views.py
+++ battle/views.py
@@ -26,10 +26,13 @@ def pokemon_details(request):
         return render(request, 'battle/pokemon_details.html', data)
 
 def fight(request):
-    pokemon_1 = request.POST.get('pk1_id')
-    pokemon_2 = request.POST.get('pk2_id')
+    p1 = request.POST.get('pk1_id')
+    p2 = request.POST.get('pk2_id')
     details_1 = Pokemon.objects.get(uid=pokemon_1)
     details_2 = Pokemon.objects.get(uid=pokemon_2)
-    
+    instances = [{"First_pokemon": p1,"Second_pokemon": p2,"p1_Type1":
+    " Fighting","p2_Type1": " Water","p1_HP": 65,"p2_HP": 170,"p1_Attack": 125,
+    "p1_Defense": 60,"p1_SpAtk": 95,"p1_SpDef": 60,"p1_Speed": 105,"p2_Attack": 90,
+    "p2_Defense": 45,"p2_SpAtk": 90,"p2_SpDef": 45,"p2_Speed": 60}]
     print(details_1)
     print(details_2)
