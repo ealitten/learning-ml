e3ffc6adca65227ddad861b311cb35b4eb6281cb
reena <reena.sanjay.sharma@gmail.com>
2018-03-14 17:32:35 +0000

    working on feature test

--- battle/test_features.py
+++ battle/test_features.py
@@ -6,8 +6,6 @@ import capybara.dsl
 import unittest
 import pokemonweb.wsgi as mywsgi
 
-
-
 capybara.app = mywsgi.application
 capybara.default_driver = "selenium"
 
@@ -18,26 +16,27 @@ class CapybaraTestCase(unittest.TestCase):
 
 
     def test_battle_page_works(self):
-        self.page.visit('index')
-        assert self.page.has_text('Welcome to We Predicted That\'s Pokemon Battle!')
+        self.page.visit('/')
+        print(self.page)
+        assert self.page.has_text("Welcome to We Predicted That's Pokemon Battle!")
+        print(self.page)
 
-    def test_pokemon_page_works(self):
-        self.pokemon = Pokemon.objects.create(uid=0, name='Charmander')
-        self.page.visit('index')
-        self.page.click_link('Your Pokemon Battle')
-        self.page.select("Charmander", field="pk1")
+    # def test_pokemon(self):
+    #     self.pokemon = Pokemon.objects.create(uid=0, name='Charmander')
+    #     self.page.visit('/pokemon_data/')
+    #     print(self.page.body)
+    #     self.page.select("Charmander", field="pk1")
 
-        assert self.page.has_content('Charmander')
+    #     assert self.page.has_content('Charmander')
 
-    def test_have_a_battle(self):
-       self.pokemon = Pokemon.objects.create(uid=1, name='Bulbasaur',type_1 = "grass")
-       self.pokemon = Pokemon.objects.create(uid=2, name='Wartortle',type_1 = "water")
-       self.page.visit('index')
-       self.page.click_link('Your Pokemon Battle')
-       self.page.select("Bulbasaur", field="pk1")
-       self.page.select("Wartortle", field="pk2")
-       self.page.click_button("Fight")
-       assert self.page.has_content("grass")
+    # def test_have_a_battle(self):
+    #    self.pokemon = Pokemon.objects.create(uid=1, name='Bulbasaur',type_1 = "grass")
+    #    self.pokemon = Pokemon.objects.create(uid=2, name='Wartortle',type_1 = "water")
+    #    self.page.select("Bulbasaur", field="pk1")
+    #    self.page.select("Wartortle", field="pk2")
+    #    self.page.click_button("Fight")
+    #    self.page.visit('/fight/')
+    #    assert self.page.has_content("Probability Mega Venusaur will win is 87% Probability Charmander will win is 13%")
 
 
     def tearDown(self):
