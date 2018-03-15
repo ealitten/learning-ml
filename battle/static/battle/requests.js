49092e28188e5598620c4a24cad58374294e3f3f
Tagrand <thomas.a.grand@gmail.com>
2018-03-15 10:33:59 +0000

    remove hidden fields

--- battle/static/battle/requests.js
+++ battle/static/battle/requests.js
@@ -9,7 +9,6 @@ $( document ).ready(function() {
        var url = $("#fightForm").attr("p-details-url"); // get the url of the `pokemon_details` view
        var selectObj = $(this);
        var pid = selectObj.val();  // get the selected pokemon ID from the HTML input
-
        $.ajax({                       // initialize an AJAX request
            url: url,                    // set the url of the request (= localhost:8000/hr/ajax/pokemon_data/)
            data: {
@@ -17,10 +16,8 @@ $( document ).ready(function() {
        },
        success: function (data) {   // `data` is the return of the `pokemon_details` view function
        var divId = `#${selectObj.attr('name')}_details`;
-       var hiddenId = `#${selectObj.attr('name')}_id`;
        var imageId = `#${selectObj.attr('name')}_img`;
        $(divId).html(data);  // replace the contents of the with the data that came from the server
-       $(hiddenId).val(pid);
        $(imageId).html(`<img src="https://storage.googleapis.com/wepredictedthat-pokemon/pokemon-images/${pid}.png">`)
       }
     })
