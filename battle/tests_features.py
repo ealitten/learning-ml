f6a9bd2c2b857b4be9947f78159757d643bad008
reena <reena.sanjay.sharma@gmail.com>
2018-03-15 16:55:10 +0000

    fixed feature tests

--- battle/tests_features.py
+++ battle/tests_features.py
@@ -20,7 +20,7 @@ class CapybaraTestCase(unittest.TestCase):
     def test_battle_page_works(self):
         self.page.visit('/')
         assert self.page.has_text(
-            "Welcome to We Predicted That's Pokemon Battle!")
+            "Battle your Pokemon!")
 
     def test_pokemon(self):
         self.pokemon = Pokemon.objects.create(uid=0, name='Charmander')
@@ -30,14 +30,14 @@ class CapybaraTestCase(unittest.TestCase):
 
     def test_have_a_battle(self):
         self.pokemon1 = Pokemon.objects.create(
-            uid=1, name='Bulbasaur', type_1="grass")
+            uid=3, name='Venusaur', type_1="grass")
         self.pokemon2 = Pokemon.objects.create(
             uid=2, name='Ivysaur', type_1="water")
         self.page.visit('/')
-        self.page.select("Bulbasaur", field="pk1")
+        self.page.select("Venusaur", field="pk1")
         self.page.select("Ivysaur", field="pk2")
-        self.page.click_button("battle")
-        assert self.page.has_text("Probability Bulbasaur will win is 44%")
+        self.page.click_button("Let's Battle")
+        assert self.page.has_text("Probability Venusaur will win is 44%")
         assert self.page.has_text("Probability Ivysaur will win is 56%")
 
     def tearDown(self):
